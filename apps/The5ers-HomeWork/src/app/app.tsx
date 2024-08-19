87754480f89e9753b72d2b672a8c19bbdcb44dab
Oron Edi <oronedi8471@gmail.com>
2024-08-19 23:34:12 +0300

    Initial commit

---
+++ apps/The5ers-HomeWork/src/app/app.tsx
@@ -0,0 +1,14 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import styles from './app.module.css';
+
+import NxWelcome from './nx-welcome';
+
+export function App() {
+  return (
+    <div>
+      <NxWelcome title="The5ers-HomeWork" />
+    </div>
+  );
+}
+
+export default App;
